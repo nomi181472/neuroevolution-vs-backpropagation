ba3622e3f9107da1ab811df56a35ba442920774d
Noman Ali <33200799+nomi181472@users.noreply.github.com>
2024-12-31 17:26:26 +0500

    map elites added

--- sample5-map_elites.py
+++ sample5-map_elites.py
@@ -18,22 +18,8 @@ from custom_operators import GreedyCrossover, MultiPointCrossOver
 from src.evotorch.operators import OnePointCrossOver
 from ensemble_model import Ensemble
 from linear_policy import LinearPolicy
+from utils import Utils
 # Utility functions
-class Utils:
-    @staticmethod
-    def set_seed(seed):
-        """Set the random seed for reproducibility."""
-        random.seed(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed_all(seed)
-
-    @staticmethod
-    def create_directory(path):
-        """Create a directory if it does not exist."""
-        if not os.path.exists(path):
-            os.makedirs(path)
 
 # Neural Network Policy
 
